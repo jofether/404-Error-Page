5939191981e07d8d13402e3e63dbee8119498692
spaghettieater <jofethersampollo@gmail.com>
2026-02-18 19:53:47 +0800

    fixed the import error

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 /* 4. TYPO BUG A: Importing the wrong CSS file name (intentional build/style fail) */
 /* FIX: import './index.css'; */
-import './index-styles.css'; 
+import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
